f032b0ab37adb95db76768e72d9bf9c9f693d109
amegeddon <ameescook@gmail.com>
2024-02-28 20:55:07 +0000

    changes init file

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -1,7 +1,5 @@
 import os
-import re
 from flask import Flask
-from flask_pymongo import PyMongo
 from flask_sqlalchemy import SQLAlchemy
 if os.path.exists("env.py"):
     import env  # noqa
@@ -9,8 +7,6 @@ if os.path.exists("env.py"):
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
-app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 
 uri = os.environ.get("DATABASE_URL")
 if uri.startswith("postgres://"):
@@ -18,6 +14,5 @@ if uri.startswith("postgres://"):
 app.config["SQLALCHEMY_DATABASE_URI"] = uri  # heroku
 
 db = SQLAlchemy(app)
-mongo = PyMongo(app)
 
 from taskmanager import routes  # noqa
