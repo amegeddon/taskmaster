0ebcc8015ffa569a04e67b89ad196e8c90efe024
amegeddon <ameescook@gmail.com>
2024-02-28 19:15:17 +0000

    typo corrected

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -12,7 +12,7 @@ if os.environ.get("DEVELOPMENT")== "True":
 else:
     uri = os.environ.get("DATABASE_URL")
     if uri.startswith("postgres://"):
-        url = url.replace("postgres://", "postgresql://", 1)
+        uri= uri.replace("postgres://", "postgresql://", 1)
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")     
 
 db = SQLAlchemy(app)
